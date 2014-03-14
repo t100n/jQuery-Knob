783ca73452f66ffec739059b967c35c259fcf316
t100n <andre@youbeq.com>
2014-03-14 10:43:17 +0000

    keep the element a square

--- js/jquery.knob.js
+++ js/jquery.knob.js
@@ -698,7 +698,8 @@
                         //,'height' : ((this.w / 3) >> 0) + 'px'
                         ,'position' : 'absolute'
                         ,'vertical-align' : 'middle'
-                        ,'margin-top' : ((this.w / 3) >> 0) + 'px'
+                        ,'margin-top' : ((this.w / 4) >> 0) + 'px'
+                        //,'margin-top' : ((this.w / 3) >> 0) + 'px'
                         ,'margin-left' : '-' + ((this.w * 3 / 4 + 2) >> 0) + 'px'
                         ,'border' : 0
                         ,'background' : 'none'
